62ae5556ed266d7630998d6e1a049abeb8bfea10
M-Ali208 <muhammedalituncar@gmail.com>
2024-12-14 20:55:42 +0300

    Character Controller (movement, camera)
    +Character movement system
    (with old input sytem)
    +Damping
    +Speedlimit
    
    +Third person view Camera System
    +Character facing to walking axis
    +third persen cam is rotating with Character

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,6 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    
+    [Header("Movement")]
+    public float moveSpeed;
+    public float Drag;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        InputSystem();
+        SpeedControl();
+        rb.linearDamping = Drag;
+    }
+    private void FixedUpdate()
+    {
+        playerMove();
+    }
+    private void InputSystem()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+    private void playerMove()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+
+        }       
+    }
 }
